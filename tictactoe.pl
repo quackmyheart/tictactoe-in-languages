45ab393e9d1b78972555e5c1fb683dbb92829449
cuboided <85214314+quackmyheart@users.noreply.github.com>
2023-10-30 21:48:09 +0900

    Update tictactoe.pl

--- tictactoe.pl
+++ tictactoe.pl
@@ -3,35 +3,41 @@ use strict;
 use warnings;
 
 # starting data structure 
-my @board = (["0", "0", "0"], ["0", "0", "0"] ,["0", "0", "0"]);
-#print @{$board[2]}[1], "\n";  
+my @board = (["0", "0", "0"], ["0", "0", "0"] ,["0", "0", "0"]); 
 
 # display the board for the user 
 sub display_board {
-    @board = @_[0]; 
-    my $i = 0; 
-    my $k = 0; 
+    my @board = @_;   
 
-    for ($i = 0; $i <= 2; $i++) {
-        for ($k = 0; $k <= 2; $k++) {
-            print $k; 
-            print $i; 
-            print @{$board[$i]}[$k], " "; 
+    for (my $i = 0; $i <= $#board; $i++){
+        for (my $k = 0; $k <= $#board; $k++) { 
+            print "$board[$i][$k] "; 
         }
-        print "\n";
-        $k = 0;
-        print $k; 
-        print $i; 
-        print "\n";
-    }
+        print "\n"; 
+    }  
+   print "\n";  
 }
 
 display_board(@board);
 
+my $turns = 0;
+while (1) {
+    if ($turns % 2 == 0){
+        print "what's your move O?\n";
+        my $move = <>; 
 
+    } else {
+        print "what's your move X?\n";
+        my $move = <>; 
+    }
+    $turns++; 
+}
+
+q{
 # take user input  
 print "What is your name?\n";
 my $name = <>;
 chomp($name);
 print "Your name is ",$name,"\n";
+}
 
